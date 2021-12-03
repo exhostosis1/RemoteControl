61bfad1c92e469a56a49fbd07180727338f6dc61
exhostosis <exhostosis@gmail.com>
2021-12-03 13:33:17 +0200

    small fixes

--- RemoteControlWinFormsCore/Logger.cs
+++ RemoteControlWinFormsCore/Logger.cs
@@ -4,8 +4,9 @@
     {
         Notify = 0,
         Warn = 1,
-        Error = 2,
-        Critical = 3
+        Debug = 2,
+        Error = 3,
+        Critical = 4
     }
 
     internal interface ILogger
@@ -16,7 +17,7 @@
 
     internal abstract class Logger
     {
-        public static ErrorLevel ErrorLevel = ErrorLevel.Error;
+        public static ErrorLevel ErrorLevel = ErrorLevel.Debug;
         public static ILogger GetFileLogger(Type type)
         {
             return new FileLogger(type);
@@ -41,13 +42,13 @@
             if (_strings.Count == 100) Flush();
 
 
-            _strings.Add((errorLevel, $"{DateTime.Now} - {errorLevel} - {_type.Name} - {message}"));
+            _strings.Add((errorLevel, $"{DateTime.Now:dd.MM.yyyy HH:mm:ss:ffff} - {errorLevel} - {_type.Name} - {message}"));
         }
 
         public void Flush()
         {
             using var sw = _file.AppendText();
-            foreach (var s in _strings.Where(x => x.Item1 >= Logger.ErrorLevel))
+            foreach (var s in _strings.Where(x => x.Item1 >= Logger.ErrorLevel).Select(x => x.Item2))
             {
                 sw.WriteLine(s);
             }
