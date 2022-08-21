a74db337b471c0f3978c3096f81988e1d850aaeb
exhostosis <exhostosis@gmail.com>
2022-08-21 15:55:10 +0300

    remove avalonia. make WinExe console frontend

--- RemoteControlConsole/Program.cs
+++ RemoteControlConsole/Program.cs
@@ -4,14 +4,14 @@ namespace RemoteControlConsole
 {
     internal static class Program
     {
-        private static void Main()
+        private static async Task Main()
         {
             var config = RemoteControlMain.Config.GetConfig();
             
             RemoteControlMain.AutostartService.SetAutostart(config.Common.Autostart);
             RemoteControlMain.Server.Start(config.UriConfig.Uri);
 
-            Console.ReadLine();
+            await Task.Delay(-1);
         }
     }
 }
