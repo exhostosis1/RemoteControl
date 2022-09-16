0f5cb286e7d76f21aa826f089f0a46a3c4a3ae68
exhostosis <exhostosis@gmail.com>
2022-09-16 14:32:24 +0300

    update

--- RemoteControlWindows/Program.cs
+++ RemoteControlWindows/Program.cs
@@ -1,33 +1,29 @@
 ﻿using System.Diagnostics;
 using System.Net;
-using System.Runtime.InteropServices;
 using System.Security.Principal;
 
-namespace RemoteControlWindows
+namespace RemoteControlWindows;
+
+public static class Program
 {
-    public static class Program
+    public static void Main()
     {
-        public static void Main()
+        var container = new RemoteControlContainer();
+        try
         {
-            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                throw new Exception("OS not supported");
+            RemoteControlMain.Main.Run(container);
+        }
+        catch (HttpListenerException)
+        {
+            var sid = new SecurityIdentifier(WellKnownSidType.BuiltinUsersSid, null);
+            var translatedValue = sid.Translate(typeof(NTAccount)).Value;
 
-            var container = new RemoteControlContainer();
-            try
-            {
-                RemoteControlMain.Main.Run(container);
-            }
-            catch (HttpListenerException)
-            {
-                var sid = new SecurityIdentifier(WellKnownSidType.BuiltinUsersSid, null);
-                var translatedValue = sid.Translate(typeof(NTAccount)).Value;
+            var args =
+                $"http add urlacl url={container.Config.GetConfig().UriConfig.Uri} user={translatedValue}";
 
-                var args =
-                    $"http add urlacl url={container.Config.GetConfig().UriConfig.Uri} user={translatedValue}";
-
-                Process.Start(new ProcessStartInfo("netsh", args) { Verb = "runas", UseShellExecute = true, CreateNoWindow = true});
-                RemoteControlMain.Main.Run(container);
-            }
+            Process.Start(new ProcessStartInfo("netsh", args)
+                {Verb = "runas", UseShellExecute = true, CreateNoWindow = true});
+            RemoteControlMain.Main.Run(container);
         }
     }
 }
