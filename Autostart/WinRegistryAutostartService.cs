fe8628700623d2176f604dc63bc3c0fd1f4f1bea
exhostosis <exhostosis@gmail.com>
2022-10-03 15:37:35 +0300

    update

--- Autostart/WinRegistryAutostartService.cs
+++ Autostart/WinRegistryAutostartService.cs
@@ -2,41 +2,33 @@
 using Shared;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Diagnostics.CodeAnalysis;
 
-namespace Autostart
+namespace Autostart;
+
+[SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "Planform check in constructor is sufficient")]
+public class WinRegistryAutostartService : IAutostartService
 {
-    public class WinRegistryAutostartService : IAutostartService
+    private readonly RegistryKey _regKey;
+    private readonly string _regName = "Remote Control";
+    private readonly string _regValue = $"\"{Process.GetCurrentProcess().MainModule?.FileName ?? throw new NullReferenceException()}\"";
+
+    public WinRegistryAutostartService()
     {
-        private readonly RegistryKey _regKey;
-        private readonly string _regName = "Remote Control";
-        private readonly string _regValue = $"\"{Process.GetCurrentProcess().MainModule?.FileName ?? throw new NullReferenceException()}\"";
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            throw new Exception("OS not supported");
 
-        public WinRegistryAutostartService()
-        {
-            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                throw new Exception("OS not supported");
+        _regKey = Registry.CurrentUser.OpenSubKey("SOFTWARE")?.OpenSubKey("Microsoft")?.OpenSubKey("Windows")
+            ?.OpenSubKey("CurrentVersion")?.OpenSubKey("Run", true) ?? throw new NullReferenceException();
+    }
+        
+    public bool CheckAutostart() => _regKey.GetValue(_regName, "") as string == _regValue;
 
-            _regKey = Registry.CurrentUser.OpenSubKey("SOFTWARE")?.OpenSubKey("Microsoft")?.OpenSubKey("Windows")
-                ?.OpenSubKey("CurrentVersion")?.OpenSubKey("Run", true) ?? throw new NullReferenceException();
-        }
+    public void SetAutostart(bool value)
+    {
+        _regKey.DeleteValue(_regName, false);
 
-        public bool CheckAutostart()
-        {
-            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                throw new Exception("OS not supported");
-
-            return _regKey.GetValue(_regName, "") as string == _regValue;
-        }
-
-        public void SetAutostart(bool value)
-        {
-            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                throw new Exception("OS not supported");
-
-            _regKey.DeleteValue(_regName, false);
-
-            if(value)
-                _regKey.SetValue(_regName, _regValue, RegistryValueKind.String);
-        }
+        if (value)
+            _regKey.SetValue(_regName, _regValue, RegistryValueKind.String);
     }
 }
