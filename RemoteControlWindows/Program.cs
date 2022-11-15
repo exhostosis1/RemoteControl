e7f61e51897df816244e06b03e5191614e75ce89
exhostosis <exhostosis@gmail.com>
2022-11-15 14:31:21 +0200

    add windows session lock check

--- RemoteControlWindows/Program.cs
+++ RemoteControlWindows/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace RemoteControlWindows;
+﻿using Microsoft.Win32;
+
+namespace RemoteControlWindows;
 
 public static class Program
 {
     public static void Main()
     {
         var container = new RemoteControlContainer();
+
+        string? listeningUri = null;
+
+        SystemEvents.SessionSwitch += (_, args) =>
+        {
+            if(args.Reason == SessionSwitchReason.SessionLock)
+            {
+                listeningUri = container.Server.IsListening ? container.Server.GetListeningUri() : null;
+                if (listeningUri != null)
+                    container.Server.Stop();
+            }
+
+            if(args.Reason == SessionSwitchReason.SessionUnlock)
+            {
+                if (listeningUri != null)
+                    container.Server.Start(new Uri(listeningUri));
+            }
+        };
 
         try
         {
