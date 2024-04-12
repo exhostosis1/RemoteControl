10b4ad28770ebe02591533f875f5722002dc28e6
exhostosis <exhostosis@gmail.com>
2024-04-12 16:06:49 +0300

    update

--- WinUI/MainWindow.xaml.cs
+++ WinUI/MainWindow.xaml.cs
@@ -10,9 +10,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using ABI.System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,34 +27,93 @@ namespace WinUI
     {
         private readonly ViewModel _viewModel = new();
 
+        private readonly ICommand _exitCommand;
+        private readonly ICommand _startAllCommand;
+        private readonly ICommand _stopAllCommand;
+        private readonly ICommand _autostartCommand;
+        private readonly ICommand _addFirewallCommand;
+
+        private FlyoutBase Flyout = new MenuFlyout();
+
         public MainWindow()
         {
             this.InitializeComponent();
+
+            _exitCommand = new RelayCommand(Exit, () => true);
+            _startAllCommand = new RelayCommand(StartAll, () => true);
+            _stopAllCommand = new RelayCommand(StopAll, () => true);
+            _autostartCommand = new RelayCommand(Autostart, () => true);
+            _addFirewallCommand = new RelayCommand(AddFirewallRule, () => true);
         }
 
-        private void StartAll(object _, RoutedEventArgs __)
+        private void StartAll()
         {
             _viewModel.StartAll();
         }
 
-        private void StopAll(object _, RoutedEventArgs __)
+        private void StopAll()
         {
             _viewModel.StopAll();
         }
 
-        private void AddFirewallRule(object _, RoutedEventArgs __)
+        private void AddFirewallRule()
         {
 
         }
 
-        private void Autostart(object sender, RoutedEventArgs __)
+        private void Autostart()
         {
-            _viewModel.SetAutostart((sender as ToggleMenuFlyoutItem)?.IsChecked ?? false);
+            _viewModel.SetAutostart(!_viewModel.IsAutostart);
         }
 
-        private void Exit(object _, RoutedEventArgs __)
+        private void Exit()
         {
             Environment.Exit(0);
+        }
+
+        private IList<MenuFlyoutItemBase> GetFlyoutItems()
+        {
+
+            //< MenuFlyoutSeparator ></ MenuFlyoutSeparator >
+            //    < MenuFlyoutItem Text = "Start all" Click = "StartAll" />
+            //    < MenuFlyoutItem Text = "Stop all" Click = "StopAll" />
+            //    < MenuFlyoutSeparator ></ MenuFlyoutSeparator >
+            //    < ToggleMenuFlyoutItem Text = "Autostart" Click = "Autostart" IsChecked = "{x:Bind _viewModel.IsAutostart}" />
+            //    < MenuFlyoutItem Text = "Add firewall rule" Click = "AddFirewallRule" />
+            //    < MenuFlyoutItem Text = "Exit" Click = "Exit" />
+
+            return
+            [
+                new MenuFlyoutSeparator(),
+                new MenuFlyoutItem
+                {
+                    Text = "Start all",
+                    Command = _startAllCommand
+                },
+                new MenuFlyoutItem
+                {
+                    Text = "Stop all",
+                    Command = _stopAllCommand
+                },
+                new MenuFlyoutSeparator(),
+                new ToggleMenuFlyoutItem
+                {
+                    Text = "Autostart",
+                    Command = _autostartCommand,
+                    IsChecked = _viewModel.IsAutostart
+                },
+                new MenuFlyoutItem
+                {
+                    Text = "Add firewall rule",
+                    Command = _addFirewallCommand
+                },
+                new MenuFlyoutItem
+                {
+                    Text = "Exit",
+                    Command = _exitCommand
+                }
+
+            ];
         }
     }
 }
