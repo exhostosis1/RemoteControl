6a08ca73e2516a0b7722d4bd035e86831a6480c2
exhostosis <exhostosis@gmail.com>
2024-04-18 20:11:57 +0300

    almost done

---
+++ WinUI/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace WinUI.Converters;
+
+internal class BoolToVisibilityConverter: IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is not bool b) return Visibility.Collapsed;
+
+        return b ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+internal class BoolToVisibilityReversed : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is not bool b) return Visibility.Collapsed;
+
+        return b ? Visibility.Collapsed : Visibility.Visible;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
--- WinUI/Views/NotificationView.xaml.cs
+++ WinUI/Views/NotificationView.xaml.cs
@@ -1,24 +1,39 @@
 using CommunityToolkit.Mvvm.Input;
 using H.NotifyIcon;
 using MainApp;
+using MainApp.Servers;
 using Microsoft.UI.Xaml;
 using System;
 using System.Linq;
-using MainApp.Servers;
-using Microsoft.UI.Xaml.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace WinUI.Views;
 
+[ObservableObject]
 public sealed partial class NotificationView
 {
     private readonly App _app;
     public readonly AppHost AppHost = ApphostProvider.AppHost;
+
+    [ObservableProperty]
+    private IServer? _firstServer;
+    [ObservableProperty]
+    private IServer? _secondServer;
 
     public NotificationView()
     {
         this.InitializeComponent();
 
         _app = Application.Current as App ?? throw new NullReferenceException();
+
+        FirstServer = AppHost.Servers.FirstOrDefault();
+        SecondServer = AppHost.Servers.Skip(1).FirstOrDefault();
+
+        AppHost.Servers.CollectionChanged += (_, _) =>
+        {
+            FirstServer = AppHost.Servers.FirstOrDefault();
+            SecondServer = AppHost.Servers.Skip(1).FirstOrDefault();
+        };
     }
 
     [RelayCommand]
@@ -33,72 +48,9 @@ public sealed partial class NotificationView
     }
 
     [RelayCommand]
-    private void OpenUri(Uri uri)
+    private void OpenUri(string uri)
     {
         AppHost.OpenSite(uri);
-    }
-
-    [RelayCommand]
-    void PopulateServers()
-    {
-        var menuItems = (TrayIcon.ContextFlyout as MenuFlyout)?.Items;
-
-        if (menuItems == null) return;
-
-        var oldItems = menuItems.Where(x => x.AccessKey != "generated").ToList();
-
-        menuItems.Clear();
-
-        foreach (var server in AppHost.Servers)
-        {
-            menuItems.Add(new MenuFlyoutItem
-            {
-                AccessKey = "generated",
-                Text = server.Config.Name
-            });
-
-            switch (server.Config.Type)
-            {
-                case ServerType.Web:
-                    menuItems.Add(new MenuFlyoutItem
-                    {
-                        AccessKey = "generated",
-                        Text = server.Config.Uri.ToString(),
-                        IsEnabled = server.Status,
-                        Command = OpenUriCommand,
-                        CommandParameter = server.Config.Uri
-                    });
-                    break;
-                case ServerType.Bot:
-                    menuItems.Add(new MenuFlyoutItem
-                    {
-                        AccessKey = "generated",
-                        Text = server.Config.UsernamesString,
-                        IsEnabled = server.Status
-                    });
-                    break;
-                default:
-                    break;
-            }
-
-            menuItems.Add(new MenuFlyoutItem
-            {
-                Text = server.Status ? "Stop" : "Start",
-                AccessKey = "generated",
-                Command = server.Status ? StopCommand : StartCommand,
-                CommandParameter = server
-            });
-
-            menuItems.Add(new MenuFlyoutSeparator
-            {
-                AccessKey = "generated"
-            });
-        }
-
-        foreach (var oldItem in oldItems)
-        {
-            menuItems.Add(oldItem);
-        }
     }
 
     [RelayCommand]
@@ -143,10 +95,5 @@ public sealed partial class NotificationView
         _app.HandleClosedEvents = false;
         TrayIcon.Dispose();
         _app.MainWindow?.Close();
-    }
-
-    private void FlyoutBase_OnOpening(object? sender, object e)
-    {
-        var a = 5;
     }
 }
