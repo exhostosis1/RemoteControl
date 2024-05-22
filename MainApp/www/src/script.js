86e98ac2c83fe7e0fbbc8b4745d035c3b71dc5f6
exhostosis <exhostosis@gmail.com>
2024-05-22 15:10:49 +0300

    update

--- MainApp/www/src/script.js
+++ MainApp/www/src/script.js
@@ -2,19 +2,23 @@ import sendRequest from './sendRequest.js';
 import initElements from './initElements.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  let pageInPortraitMode = window.innerHeight > window.innerWidth;
+  let pageInPortraitMode;
+
+  function setAttribute() {
+    pageInPortraitMode = window.innerHeight > window.innerWidth;
+    document.querySelector('meta[name=viewport]').setAttribute('content', `width=${window.innerWidth}, height=${window.innerHeight}, initial-scale=1.0, maximum-scale=1.0, user-scalable=0`);
+  }
+
+  setAttribute();
 
   window.addEventListener('resize', () => {
     if (((pageInPortraitMode === true)
         && (window.innerHeight < window.innerWidth))
         || ((pageInPortraitMode === false)
         && (window.innerHeight > window.innerWidth))) {
-      pageInPortraitMode = window.innerHeight > window.innerWidth;
-      document.querySelector('meta[name=viewport]').setAttribute('content', `width=${window.innerWidth}, height=${window.innerHeight}, initial-scale=1.0, maximum-scale=1.0, user-scalable=0`);
+      setAttribute();
     }
   });
-
-  document.querySelector('meta[name=viewport]').setAttribute('content', `width=${window.innerWidth}, height=${window.innerHeight}, initial-scale=1.0, maximum-scale=1.0, user-scalable=0`);
 
   const devices = JSON.parse(await (await sendRequest('audio', 'getdevices')).text());
   const volume = await (await sendRequest('audio', 'getvolume')).text();
